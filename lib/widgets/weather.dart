a921c48cd43308e9c1324f096e34768e2e27e425
Rahul Karkera <rhlk92@gmail.com>
2020-07-05 20:33:17 +0530

    Add widgets for the weather app

---
+++ lib/widgets/weather.dart
@@ -0,0 +1,107 @@
+import 'package:FlutterWeather/bloc/weather_bloc.dart';
+import 'package:FlutterWeather/bloc/weather_event.dart';
+import 'package:FlutterWeather/bloc/weather_state.dart';
+import 'package:FlutterWeather/repositories/repositories.dart';
+import 'package:FlutterWeather/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class Weather extends StatefulWidget {
+  final WeatherRepository weatherRepository;
+
+  Weather({Key key, @required this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(key: key);
+
+  @override
+  State<Weather> createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+  WeatherBloc _weatherBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherBloc = WeatherBloc(weatherRepository: widget.weatherRepository);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Weather'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              final city = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CitySelection(),
+                ),
+              );
+              if (city != null) {
+                _weatherBloc.dispatch(FetchWeather(city: city));
+              }
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: BlocBuilder(
+          bloc: _weatherBloc,
+          builder: (_, WeatherState state) {
+            if (state is WeatherEmpty) {
+              return Center(child: Text('Please Select a Location'));
+            }
+            if (state is WeatherLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (state is WeatherLoaded) {
+              final weather = state.weather;
+
+              return ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 100.0),
+                    child: Center(
+                      child: Location(location: weather.location),
+                    ),
+                  ),
+                  Center(
+                    child: LastUpdated(dateTime: weather.lastUpdated),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 50.0),
+                    child: Center(
+                      child: CombinedWeatherTemperature(
+                        weather: weather,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+            if (state is WeatherError) {
+              return Text(
+                'No weather data!',
+                style: TextStyle(color: Colors.red),
+              );
+            }
+            return Text(
+              'Something went wrong!',
+              style: TextStyle(color: Colors.red),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _weatherBloc.dispose();
+    super.dispose();
+  }
+}
