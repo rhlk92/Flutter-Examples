6d4172e258565402d34c7a502b8125e3380c6cb9
Rahul Karkera <rhlk92@gmail.com>
2020-07-10 02:45:00 +0530

    Add bloc for authentication

---
+++ lib/bloc/authentication/authentication_bloc.dart
@@ -0,0 +1,40 @@
+import 'package:FlutterWeather/bloc/authentication/authentication_event.dart';
+import 'package:FlutterWeather/bloc/authentication/authentication_state.dart';
+import 'package:FlutterWeather/repositories/user/user_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+
+  AuthenticationBloc({@required this.userRepository})
+      : assert(userRepository != null),
+        super(AuthenticationInitial());
+
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    if (event is AuthenticationStarted) {
+      final bool hasToken = await userRepository.hasToken();
+
+      if (hasToken) {
+        yield AuthenticationSuccess();
+      } else {
+        yield AuthenticationFailure();
+      }
+    }
+
+    if (event is AuthenticationLoggedIn) {
+      yield AuthenticationInProgress();
+      await userRepository.persistToken(event.token);
+      yield AuthenticationSuccess();
+    }
+
+    if (event is AuthenticationLoggedOut) {
+      yield AuthenticationInProgress();
+      await userRepository.deleteToken();
+      yield AuthenticationFailure();
+    }
+  }
+}
